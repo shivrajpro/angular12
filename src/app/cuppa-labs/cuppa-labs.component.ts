ad21fd17c5de24df46dc831672f7678bbb03ce51
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-07-01 11:08:38 +0530

    adding 2 dropdowns side by side

--- src/app/cuppa-labs/cuppa-labs.component.ts
+++ src/app/cuppa-labs/cuppa-labs.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-interface Country{
-  id:number,
-  itemName:string
+interface Country {
+  id: number,
+  itemName: string
 }
 @Component({
   selector: 'app-cuppa-labs',
@@ -10,50 +10,50 @@ interface Country{
   styleUrls: ['./cuppa-labs.component.scss']
 })
 export class CuppaLabsComponent implements OnInit {
-  dropdownList:Country[] = [];
+  dropdownList: Country[] = [];
   selectedItems: Country[] = [];
   dropdownSettings = {};
-  ngOnInit(){
-      this.dropdownList = [
-                            {"id":1,"itemName":"India"},
-                            {"id":2,"itemName":"Singapore"},
-                            {"id":3,"itemName":"Australia"},
-                            {"id":4,"itemName":"Canada"},
-                            {"id":5,"itemName":"South Korea"},
-                            {"id":6,"itemName":"Germany"},
-                            {"id":7,"itemName":"France"},
-                            {"id":8,"itemName":"Russia"},
-                            {"id":9,"itemName":"Italy"},
-                            {"id":10,"itemName":"Sweden"}
-                          ];
-      this.selectedItems = [
-                              {"id":2,"itemName":"Singapore"},
-                              {"id":3,"itemName":"Australia"},
-                              {"id":4,"itemName":"Canada"},
-                              {"id":5,"itemName":"South Korea"}
-                          ];
-      this.dropdownSettings = { 
-                                singleSelection: false, 
-                                text:"Select Countries",
-                                selectAllText:'Select All',
-                                unSelectAllText:'UnSelect All',
-                                enableSearchFilter: true,
-                                classes:"myclass custom-class"
-                              };            
+  ngOnInit() {
+    this.dropdownList = [
+      { "id": 1, "itemName": "India" },
+      { "id": 2, "itemName": "Singapore" },
+      { "id": 3, "itemName": "Australia" },
+      { "id": 4, "itemName": "Canada" },
+      { "id": 5, "itemName": "South Korea" },
+      { "id": 6, "itemName": "Germany" },
+      { "id": 7, "itemName": "France" },
+      { "id": 8, "itemName": "Russia" },
+      { "id": 9, "itemName": "Italy" },
+      { "id": 10, "itemName": "Sweden" }
+    ];
+    this.selectedItems = [
+      { "id": 2, "itemName": "Singapore" },
+      { "id": 3, "itemName": "Australia" },
+      { "id": 4, "itemName": "Canada" },
+      { "id": 5, "itemName": "South Korea" }
+    ];
+    this.dropdownSettings = {
+      singleSelection: true,
+      text: "Select Countries",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      badgeShowLimit: 1
+    };
   }
-  onItemSelect(item:any){
-      console.log(item);
-      console.log(this.selectedItems);
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
   }
-  OnItemDeSelect(item:any){
-      console.log(item);
-      console.log(this.selectedItems);
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
   }
-  onSelectAll(items: any){
-      console.log(items);
+  onSelectAll(items: any) {
+    console.log(items);
   }
-  onDeSelectAll(items: any){
-      console.log(items);
+  onDeSelectAll(items: any) {
+    console.log(items);
   }
 
 }
