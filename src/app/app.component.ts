e02053363f5951cf01ea5b32c2b1c237a180aa32
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-12-06 12:08:06 +0530

    setup jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { MsalService } from '@azure/msal-angular';
-import { FilterItem } from './data/custom-filters-data';
-import { UsersService } from './services-assignment/services/users.service';
 
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [UsersService]
+  providers: []
 })
 export class AppComponent implements OnInit {
   title = 'my-app';
-  loginForm: FormGroup;
-  emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
-  editing = false;
-  fcValue = "test";
-  numbers:any[] = [];
 
-  constructor(
-    private formBuilder: FormBuilder  ){}
+  constructor(){}
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
-      password: [null, Validators.required]
-    });
-  }
 
-  setError(){
-    this.loginForm.controls.email.setErrors({blank: true});
-  }
-  removeError(){
-    this.loginForm.controls.email.setErrors({blank: null});
-  }
-
-  submit() {
-    if (!this.loginForm.valid) {
-      return;
-    }
-    console.log(this.loginForm.value);
-  }  
-
-  inputRec(data:any){
-    console.log('input rec in app cmp=',data);
-    this.fcValue = data;
-  }
-
-  onReceiveNumber(data:any){
-    console.log('data',data);
-    this.numbers.push(data);
   }
 }
